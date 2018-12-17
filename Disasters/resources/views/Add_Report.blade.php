0d553b4f93d7fdab9f92f5ecdec4a1e3d727a39b
Mohamed Shawky <mohamedshawky911@gmail.com>
2018-12-17 21:01:32 +0200

    Update Add_Report.blade.php

--- Disasters/resources/views/Add_Report.blade.php
+++ Disasters/resources/views/Add_Report.blade.php
@@ -3,17 +3,20 @@
 
 @section('content3')
 
-      <h1>Insert a Report</h1>
+      <h1>Submit a Report</h1>
       <div class="col-sm-6 text-left">
 
       </div>
         <hr>
         <h3>Write your Report</h3>
       <div class="form-group" style="margin-top:17px">
-          <textarea class="form-control" rows="13" required></textarea>
-          <p></p>
+          <form method="post" action="Add_Report">
+
+          <textarea class="form-control" type="text" name="usrname" rows="13" required>Enter your text here...</textarea>
 
           <input type="submit" class="btn btn-success"></input>
+
+          </form>
         </div>
 
         @endsection
